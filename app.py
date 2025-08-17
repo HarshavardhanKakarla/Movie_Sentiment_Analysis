68dd355f0dd3a4eb3c057d49d07a715217bb8ef5
KAKARLA HARSHAVARDHAN <140223982+HarshavardhanKakarla@users.noreply.github.com>
2025-08-17 15:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import re
 import nltk
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
